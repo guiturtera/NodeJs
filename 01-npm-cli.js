6b48c6e19f076eff21fc5b2f1b0c693c6a626143
Guilherme Turtera <guiturtera@hotmail.com>
2021-09-21 17:47:12 -0300

    first coommit

---
+++ 01-npm-cli.js
@@ -0,0 +1,17 @@
+// npm - global command, comes with node
+// npm --version
+
+// local dependency - use it only in this particular project
+// npm i <package-name>
+
+// global dependency - use it in any project
+// npm install -g <package-name> ----- PREFER TO SET (VENV IN PYTHON)
+
+// package.json - manifest file (stores important info about project/packages)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything by default)
+const _ = require('lodash')
+const items = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
